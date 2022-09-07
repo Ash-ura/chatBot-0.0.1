dd617cd7f1bf30a8ac23fab2e39b7c0d929a9961
Ash-ura <adagbesenam@gmail.com>
2022-09-07 17:40:42 +0000

    Accomodated more salutation patterns

--- src/main/java/com/itconsortiumgh/AddNewResponse.java
+++ src/main/java/com/itconsortiumgh/AddNewResponse.java
@@ -1,2 +1,37 @@
-package com.itconsortiumgh;public class AddAiml {
+package com.itconsortiumgh;
+
+import java.io.File;
+
+import org.alicebot.ab.Bot;
+import org.alicebot.ab.MagicBooleans;
+
+public class AddNewResponse {
+
+    private static final boolean TRACE_MODE = false;
+    static String botName = "Araba";
+
+    public static void main(String[] args) {
+        try {
+
+            String resourcesPath = getResourcesPath();
+            System.out.println(resourcesPath);
+            MagicBooleans.trace_mode = TRACE_MODE;
+            Bot bot = new Bot("Araba", resourcesPath);
+
+            bot.writeAIMLFiles();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String getResourcesPath() {
+        File currDir = new File(".");
+        String path = currDir.getAbsolutePath();
+        path = path.substring(0, path.length() - 2);
+        System.out.println(path);
+        return path + File.separator + "src"
+                + File.separator + "main" + File.separator + "resources";
+    }
+
 }
